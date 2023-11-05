8fa0c3276994d03c7855a59d92114ba4e289e4b3
ayushadarshgit <adarshayush9939@gmail.com>
2023-11-05 12:33:59 +0530

    frontend setup

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
 
 
-const app = repress()
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("Sending file from port 4000")
+})
+
+app.listen(4000,()=>{
+    console.log("Listening on port 4000")
+})
